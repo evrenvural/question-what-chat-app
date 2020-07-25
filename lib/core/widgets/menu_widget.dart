80b6935aeadfc6cff111e830530e1d2b0c580e1d
Halit Aksoy <jhalitaksoy@gmail.com>
2020-07-25 18:55:00 +0300

    Completed menu_widget

--- lib/core/widgets/menu_widget.dart
+++ lib/core/widgets/menu_widget.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:group_6/core/category/category_list.dart';
 
-class MeduWidget extends StatelessWidget {
-  const MeduWidget({Key key}) : super(key: key);
+class MenuWidget extends StatelessWidget {
+  final OnCategorySelected onCategorySelected;
+
+  const MenuWidget({Key key, this.onCategorySelected}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text("Menu");
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          buildTopImage(),
+          Expanded(child: CategoryList()),
+        ],
+      ),
+    );
+  }
+
+  Stack buildTopImage() {
+    return Stack(
+      children: <Widget>[
+        AspectRatio(
+          aspectRatio: 1,
+          child: Image.asset(
+            "assets/background.png",
+            fit: BoxFit.cover,
+          ),
+        ),
+        AspectRatio(
+          aspectRatio: 1,
+          child: Image.asset(
+            "assets/robot_profile.gif",
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../core/widgets/menu_widget.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Home View"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),
+        drawer: MenuWidget(
+          onCategorySelected: (category) {
+            print("Category selected : " + category.name);
+          },
+        ),
+        body: Center(
+          child: Text("Home View"),
+        ),
       ),
     );
   }

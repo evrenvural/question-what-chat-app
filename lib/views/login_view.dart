b0ef5d68d38b0dd762ac3ad88b8af787265cb96f
evrenvural <evrenvural4@gmail.com>
2020-07-25 19:53:05 +0300

    login_view redesigned

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -6,8 +6,33 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Login View"),
+      backgroundColor: Color(0xffe9e2f5),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Flexible(
+            flex: 3,
+            child: Container(
+                decoration: BoxDecoration(
+              image: DecorationImage(
+                alignment: Alignment.topCenter,
+                image: AssetImage("assets/background.png"),
+                fit: BoxFit.fill,
+              ),
+            )),
+          ),
+          Flexible(
+            flex: 2,
+            child: Container(
+                decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xffe9e2f5), Colors.white],
+              ),
+            )),
+          )
+        ],
       ),
     );
   }

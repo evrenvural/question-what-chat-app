2387952a9d866a93e8165ccfc6fec9a1ce0e6975
İlayda Kayım <ilaydakayim99@hotmail.com>
2020-07-27 23:23:39 +0300

    ik welcomePage

--- lib/views/welcome_view.dart
+++ lib/views/welcome_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:group_6/core/my_colors.dart';
+import 'package:group_6/core/widgets/rounded_button_widget.dart';
 
 class WelcomeView extends StatelessWidget {
   const WelcomeView({Key key}) : super(key: key);
@@ -6,9 +8,50 @@ class WelcomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Welcome View"),
+      backgroundColor: Color(0xffe9e2f5),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Flexible(flex: 4, child: buildTopContainer(context)),
+          Flexible(flex: 2, child: buildButtonContainer(context))
+        ],
       ),
     );
   }
+}
+
+Container buildTopContainer(context) {
+  return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          alignment: Alignment.topCenter,
+          image: AssetImage("assets/robotgif.gif"),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: buildButtonContainer(context));
+}
+
+Container buildButtonContainer(context) {
+  return Container(
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xffe9e2f5), Colors.white]),
+    ),
+    child: buildSignInButton(context),
+  );
+}
+
+Center buildSignInButton(context) {
+  return Center(
+    child: RoundedButtonWidget(
+      toolTip: "welcome",
+      color: MyColors.BLUE,
+      text: "welcome",
+      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 14.0),
+      onPressed: () {},
+    ),
+  );
 }

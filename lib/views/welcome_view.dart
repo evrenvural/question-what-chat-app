3a3e01644eea204b22625c465023232dd93c070f
evrenvural <evrenvural4@gmail.com>
2020-07-28 11:19:56 +0300

    welcome page was completed

--- lib/views/welcome_view.dart
+++ lib/views/welcome_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:group_6/core/my_colors.dart';
 import 'package:group_6/core/widgets/rounded_button_widget.dart';
 
@@ -9,50 +10,35 @@ class WelcomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffe9e2f5),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Flexible(flex: 4, child: buildTopContainer(context)),
-          Flexible(flex: 2, child: buildButtonContainer(context))
-        ],
+      body: buildLogoContainer(context),
+    );
+  }
+
+  Container buildLogoContainer(context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          alignment: Alignment.topCenter,
+          image: AssetImage("assets/robotgif.gif"),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: buildSignInButton(context),
+    );
+  }
+
+  Container buildSignInButton(context) {
+    return Container(
+      margin:
+          EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.05),
+      alignment: Alignment.bottomCenter,
+      child: RoundedButtonWidget(
+        toolTip: "Welcome",
+        color: MyColors.BLUE,
+        text: "WELCOME",
+        padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 42.0),
+        onPressed: () {},
       ),
     );
   }
-}
-
-Container buildTopContainer(context) {
-  return Container(
-    decoration: BoxDecoration(
-      image: DecorationImage(
-        alignment: Alignment.topCenter,
-        image: AssetImage("assets/robotgif.gif"),
-        fit: BoxFit.fill,
-      ),
-    ),
-    //child: buildSignInButton(context)
-  );
-}
-
-Container buildButtonContainer(context) {
-  return Container(
-    decoration: BoxDecoration(
-      gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [Color(0xffe9e2f5), Colors.white]),
-    ),
-    child: buildSignInButton(context),
-  );
-}
-
-Center buildSignInButton(context) {
-  return Center(
-    child: RoundedButtonWidget(
-      toolTip: "WELCOME",
-      color: MyColors.BLUE,
-      text: "WELCOME",
-      padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-      onPressed: () {},
-    ),
-  );
 }
